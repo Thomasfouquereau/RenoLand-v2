609ab911ddcc819c4fb44da410314c9236cb9cc2
thomas fouquereau <thomas.fouquereau@gmail.com>
2024-04-02 11:52:12 +0200

    first commit

---
+++ js/navMenu.js
@@ -0,0 +1,19 @@
+const toggleMenu = () => {
+    const navButton = document.getElementById('NavMenuMobileButton');
+    const navMenuMobile = document.getElementById('NavMenuLinkMobile');
+    const navMenuMobileIcon = document.getElementById('NavMenuMobileIcon');
+    const navButtonText = document.querySelector('#NavMenuMobileText');
+
+    navButton.addEventListener('click', () => {
+        navMenuMobile.classList.toggle('open');
+        if (navMenuMobile.classList.contains('open')) {
+            navMenuMobileIcon.src = "/addons/IconClose.svg";
+            navButtonText.textContent = "fermer";
+        } else {
+            navMenuMobileIcon.src = "/addons/IconMenu.svg";
+            navButtonText.textContent = "menu";
+        }
+    });
+};
+
+window.addEventListener('DOMContentLoaded', toggleMenu);
